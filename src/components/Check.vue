76d8708415635ded9b483f495c0779ab7d89050d
Matej Kňazík <mato.knazik@gmail.com>
2019-08-09 20:04:15 +0200

    check examples

--- src/components/Check.vue
+++ src/components/Check.vue
@@ -30,12 +30,34 @@
     </div>
     <div class="body">
       <p>{{ getCheckData(checkKey).description }}</p>
-      <p v-if="getCheckData(checkKey).exampleImage">
-        <img
-          class="example"
-          :src="'/examples/' + getCheckData(checkKey).exampleImage  + '.png'"
-        />
-      </p>
+      <div class="example">
+        <div class="line" v-for="(translation, loc) in examples" :key="loc">
+          <div class="loc">{{ loc }}</div>
+          <div class="flag-icon"></div>
+          <div class="translation">{{ translation.text }}</div>
+        </div>
+      </div>
+      <div class="levels">
+        <div class="label">
+          <p>Check severity level:</p>
+        </div>
+        <div
+          v-for="level in ['suggestion', 'warning', 'error']"
+          :key="level"
+          class="radios"
+        >
+          <b-form-radio
+            v-model="getCheckData(checkKey).level"
+            :value="level"
+            @change.native="setCheckLevel({ check: checkKey, level })"
+            class="radio"
+          >
+          </b-form-radio>
+          <div class="radio-label">
+            {{ level.charAt(0).toUpperCase() + level.slice(1) }}
+          </div>
+        </div>
+      </div>
     </div>
   </div>
 </template>
@@ -48,6 +70,42 @@ export default {
   inheritAttrs: false,
   props: {
     checkKey: { type: String, required: true },
+  },
+  data() {
+    return {
+      examples: {
+        "es-ES": {
+          text: "Podemos proporcionar <b>__availableOfficers__</b> oficiales para asegurar su evento. " +
+                "También cubriremos todos los daños causados por ladrones durante su evento.",
+          lastCharType: ["dot", "dot"],
+          firstCharType: ["letter", "letter"],
+          placeholders: ["__availableOfficers__"],
+          tags: ["<b>", "</b>"],
+        },
+        "en-GB": {
+          text: "We are able to to secure yuor event with <b>__availableOfficers__</b> policemen.",
+          style: [{ index: 15, offset: 2, reason: "\"to\" is repeated" }],
+          insensitiveness: ["`policemen` may be insensitive, use `officers`, `police officers` instead"],
+          typos: ["yuor"],
+          lastCharType: ["dot", "dot"],
+          firstCharType: ["letter", "letter"],
+          placeholders: ["__availableOfficers__"],
+          tags: ["<b>", "</b>"],
+        },
+        "fr-FR": {
+          text: "- Nous sommes en mesure de sécuriser votre événement avec <u>4</u> policiers",
+          lastCharType: ["letter", "dot"],
+          firstCharType: ["dash", "letter"],
+          disallowedTags: ["<u>", "</u>"],
+        },
+        "zh-CN": {
+          text: "我们能够通过__availableOfficers__名警察确保您的活动安全。",
+          lastCharType: ["dot", "dot"],
+          firstCharType: ["letter", "letter"],
+          placeholders: ["__availableOfficers__"],
+        },
+      },
+    }
   },
   computed: {
     ...mapGetters([
@@ -66,6 +124,7 @@ export default {
   methods: {
     ...mapMutations([
       "setCheckActiveness",
+      "setCheckLevel",
     ]),
     getIcon(checkKey) {
       return `${checkKey.replace(/.*_/g, "")}Icon`
@@ -129,6 +188,7 @@ export default {
   .body {
     clear: both;
     margin-top: 10px;
+    width: 400px;
     color: hsl(0, 0%, 30%);
     font-size: 80%;
   }
@@ -137,5 +197,28 @@ export default {
     padding: 10px;
     border: 1px solid lightgray;
     border-radius: 5px;
+  }
+  .levels {
+    display: flex;
+    flex-direction: row;
+    justify-content: center;
+    align-items: center;
+  }
+  .levels .label {
+    vertical-align: center;
+  }
+  .levels .label p {
+    margin: 0 !important;
+  }
+  .levels .radios {
+    flex-grow: 2;
+    text-align: center;
+    justify-content: center;
+  }
+  .radio-label {
+    font-size: 9px;
+  }
+  .levels .custom-control {
+    padding-left: 1.85rem
   }
 </style>
