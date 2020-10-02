f9f4307fcf0919de201b76c084b2659fb00e8ebb
Matej <mato.knazik@gmail.com>
2020-10-02 13:32:00 +0200

    feat(auto-recovery): prevention of mutex getting stuck caused by google cloud function timeout

--- functions/dbMutex.js
+++ functions/dbMutex.js
@@ -1,12 +1,15 @@
 const database = require("./database")
+const isUpdatingTakingLongerThan = require("./helpers/isUpdatingTakingLongerThan")
 
 async function tryLock(status) {
   const updateInProgress = (await database.ref("/updateInProgress").once("value")).val()
-  if (updateInProgress) {
+  if (updateInProgress && isUpdatingTakingLongerThan(10, updateInProgress.info.timestamp)) {
+    await database.ref("/updateInProgress").remove()
+  } else if (updateInProgress) {
     return false
   }
   console.log(`Setting updateInProgress to state: ${status}`)
-  await database.ref("/updateInProgress/info").set({ state: status })
+  await database.ref("/updateInProgress/info").set({ state: status, timestamp: new Date().toUTCString() })
   return true
 }
 
